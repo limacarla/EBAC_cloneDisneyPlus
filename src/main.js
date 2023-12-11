e253dcd088ddea1c287c67de42ea79aaebf94f12
Carla <carlalima@exemplo.com>
2023-12-11 19:31:01 -0300

    criando seção de planos

--- src/main.js
+++ src/main.js
@@ -8,12 +8,22 @@ document.addEventListener('DOMContentLoaded', function() {
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
 })
 
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
+
 function escondeTodasAbas() {
-    const tabsContainer = document.querySelectorAll('data-tab-id');
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
